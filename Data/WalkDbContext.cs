5b683cd250e4eb516f065c2c7340e54bcaea766a
joasdev <joeaspiazudeveloper@gmail.com>
2023-03-21 11:11:55 -0500

    Create domain, install nucket packages for EF Core, sql server management, etc and run migrations

---
+++ Data/WalkDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPIWalkJA.Models.Domain;
+
+namespace WebAPIWalkJA.Data
+{
+	public class WalkDbContext: DbContext
+    {
+		public WalkDbContext(DbContextOptions<WalkDbContext> options): base(options)
+		{
+		}
+
+		public DbSet<Region> Region { get; set; }
+
+        public DbSet<Walk> Walk { get; set; }
+
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+
+    }
+}
+
---
+++ Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace WebAPIWalkJA.Models.Domain
+{
+	public class WalkDifficulty
+	{
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+    }
+}
+
